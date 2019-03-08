c1ee88e90c3de8673f9ae7887cac684e365873ce
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-03-08 11:16:50 +0000

    Create first segmentation Algorithm, simple Otsu

---
+++ Data/segmentationOtsu.m
@@ -0,0 +1,19 @@
+function  [dataOut,Jacc] = segmentationOtsu(dataIn,GT)
+
+
+%% Take only red channel, filter to remove backgroun
+dataIn_2    = dataIn(:,:,1);
+dataIn_3    = imfilter(dataIn_2,gaussF(19,19,1),'replicate');
+dataIn_4    = (dataIn_2-dataIn_3);
+
+%%
+thresLevel  = 255 *graythresh(dataIn_4);
+dataOut     = dataIn_4>thresLevel;
+
+
+%% Measure Jaccard Index if GT available
+if exist('GT','var')
+    Jacc = sum(sum(dataOut&GT)) / sum(sum(dataOut|GT));
+else
+    Jacc = [];
+end
