e758f02309ae9d85cc9cfc9871377d3cb345a443
CCRA <ccr22@cm3-1-01-22.managed.city.ac.uk>
2019-03-18 16:38:46 +0000

    segmentation with intensity and size

--- CODE/segmentationGlistenings.m
+++ CODE/segmentationGlistenings.m
@@ -9,8 +9,18 @@ function  [dataOut,Jacc,HitRate] = segmentationGlistenings(dataIn,GT,OtsuT)
 
 %% filter with a median filter to obtain a uniform region
 S1_2                        = medfilt2(S1_1,15*[1 1]);
+S1_2B                       = medfilt2(a1,15*[1 1]);
+%S1_2B                       = imfilter(a1,gaussF(29,29,1),'replicate');
+%% Segmentation of the peaks by edges
 
-%% obtain the mean of the filtered version
+dataIn_backRemoved          = a1 - S1_2B;
+dataIn_waters               = watershed(255-imfilter(dataIn_backRemoved,gaussF(3,3,1),'replicate'));
+preliminaryPeaks            = (double(dataIn_backRemoved) .*(dataIn_waters>0));
+
+
+%edgesData                   = (edge(dataIn_backRemoved,'canny',[],0.5));
+%edgesFilled                 = imfill(edgesData,'holes');
+ %peaks                      = edgesFilled - edgesData;
 %S1_3                        = mean(S1_2);
 
 
@@ -81,48 +91,159 @@ posMin_1R            = find(avInt_1R<minInt_1R,1,'first');
 % axis tight
 
 %% Find the actual strip by thresholding the distance transform
-S1_10L      = (S1_9L<posMin_1L).*(1-regionIris);
+S1_10Left      = (S1_9L<posMin_1L).*(1-regionIris);
 % Regions may overlap, ensure this does not happen by removing the first region
 % calculated
-S1_10R      = (S1_9R<posMin_1R).*(1-S1_10L).*(1-regionIris);
+S1_10Right      = (S1_9R<posMin_1R).*(1-S1_10Left).*(1-regionIris);
 
 %% Find the intermediate region and the others
-BackgroundRegions   = bwlabel(1-(S1_10L|S1_10R|regionIris));
+BackgroundRegions   = bwlabel(1-(S1_10Left|S1_10Right|regionIris));
 
 centralR            = unique(BackgroundRegions.*S1_8B);
 centralR(centralR==0)=[];
 
 
 
-S1_10Cent           = (BackgroundRegions==centralR);
-S1_10Edges          = (BackgroundRegions>0) - S1_10Cent;
+S1_10Centre           = (BackgroundRegions==centralR);
+S1_10Edges          = (BackgroundRegions>0) - S1_10Centre;
+%% Proceed to segment but set thresholds per region
+
+data_Edges          = preliminaryPeaks(S1_10Edges>0);
+data_Left           = preliminaryPeaks(S1_10Left>0);
+data_Centre         = preliminaryPeaks(S1_10Centre>0);
+data_Right          = preliminaryPeaks(S1_10Right>0);
+
+data_Edges          = data_Edges(data_Edges>0);
+data_Left           = data_Left(data_Left >0);
+data_Centre         = data_Centre (data_Centre>0) ;
+data_Right          = data_Right(data_Right >0);
+
+data_Edges_S        = sort(data_Edges);
+data_Left_S         = sort(data_Left);
+data_Centre_S       = sort(data_Centre);
+data_Right_S        = sort(data_Right);
+data_Edges_N        = numel(data_Edges);
+data_Left_N         = numel(data_Left);
+data_Centre_N       = numel(data_Centre);
+data_Right_N        = numel(data_Right);
+
+levThres            = 0.95;
+thres_Edges         = data_Edges_S (round(levThres*data_Edges_N));
+thres_Left          = data_Left_S  (round(levThres*data_Left_N));
+thres_Centre        = data_Centre_S(round(levThres*data_Centre_N));
+thres_Right         = data_Right_S (round(levThres*data_Right_N));
+
+% thres_Edges         = 255 *graythresh(uint8(data_Edges(data_Edges>0)));
+% thres_Left          = 255 *graythresh(uint8(data_Left(data_Left >0)));
+% thres_Centre        = 255 *graythresh(uint8(data_Centre (data_Centre>0) ));
+% thres_Right         = 255 *graythresh(uint8(data_Right(data_Right >0)));
+
+%% Segment and discard small
+peaks_Edges          = bwlabel((preliminaryPeaks.*S1_10Edges)>thres_Edges);
+peaks_Left           = bwlabel((preliminaryPeaks.*S1_10Left)>thres_Left);
+peaks_Centre         = bwlabel((preliminaryPeaks.*S1_10Centre)>thres_Centre);
+peaks_Right          = bwlabel((preliminaryPeaks.*S1_10Right)>thres_Right);
+
+peaks_Edges_R       = regionprops(peaks_Edges,preliminaryPeaks,'Area','meanIntensity');
+peaks_Left_R        = regionprops(peaks_Left,preliminaryPeaks,'Area','meanIntensity');
+peaks_Centre_R      = regionprops(peaks_Centre,preliminaryPeaks,'Area','meanIntensity');
+peaks_Right_R       = regionprops(peaks_Right,preliminaryPeaks,'Area','meanIntensity');
+
+minInt_Edges        = std([peaks_Edges_R.MeanIntensity]) +mean([peaks_Edges_R.MeanIntensity]);
+minInt_Left         = std([peaks_Left_R.MeanIntensity]) +mean([peaks_Left_R.MeanIntensity]);
+minInt_Centre       = std([peaks_Centre_R.MeanIntensity]) +mean([peaks_Centre_R.MeanIntensity]);
+minInt_Right        = std([peaks_Right_R.MeanIntensity]) +mean([peaks_Right_R.MeanIntensity]);
+
+minSize             = 15;
+% relatively Bright and large
+peaks_Edges_2       = ismember(peaks_Edges,find(([peaks_Edges_R.Area]>minSize)&([peaks_Edges_R.MeanIntensity]>minInt_Edges)));
+peaks_Left_2       = ismember(peaks_Left,find(([peaks_Left_R.Area]>minSize)&([peaks_Left_R.MeanIntensity]>minInt_Left)));
+peaks_Centre_2       = ismember(peaks_Centre,find(([peaks_Centre_R.Area]>minSize)&([peaks_Centre_R.MeanIntensity]>minInt_Centre)));
+peaks_Right_2       = ismember(peaks_Right,find(([peaks_Right_R.Area]>minSize)&([peaks_Right_R.MeanIntensity]>minInt_Right)));
+
+minInt_Edges        = 1.5*std([peaks_Edges_R.MeanIntensity]) +mean([peaks_Edges_R.MeanIntensity]);
+minInt_Left         = 1.5*std([peaks_Left_R.MeanIntensity]) +mean([peaks_Left_R.MeanIntensity]);
+minInt_Centre       = 1.5*std([peaks_Centre_R.MeanIntensity]) +mean([peaks_Centre_R.MeanIntensity]);
+minInt_Right        = 1.5*std([peaks_Right_R.MeanIntensity]) +mean([peaks_Right_R.MeanIntensity]);
+
+% very Bright but small
+minSize             = 5;
+peaks_Edges_3       = ismember(peaks_Edges,find(([peaks_Edges_R.Area]>minSize)&([peaks_Edges_R.MeanIntensity]>minInt_Edges)));
+peaks_Left_3        = ismember(peaks_Left,find(([peaks_Left_R.Area]>minSize)&([peaks_Left_R.MeanIntensity]>minInt_Left)));
+peaks_Centre_3      = ismember(peaks_Centre,find(([peaks_Centre_R.Area]>minSize)&([peaks_Centre_R.MeanIntensity]>minInt_Centre)));
+peaks_Right_3       = ismember(peaks_Right,find(([peaks_Right_R.Area]>minSize)&([peaks_Right_R.MeanIntensity]>minInt_Right)));
+
+allPeaks            =   peaks_Edges_2 + peaks_Left_2 + peaks_Centre_2 + peaks_Right_2 + ...
+                        peaks_Edges_3 + peaks_Left_3 + peaks_Centre_3 + peaks_Right_3;
+%%
+% figure(10)
+% subplot(151)
+% imagesc(dataIn_backRemoved.*(uint8(S1_10Edges)))
+% colorbar
+% subplot(152)
+% imagesc(dataIn_backRemoved.*(uint8(S1_10Left)))
+% colorbar
+% subplot(153)
+% imagesc(dataIn_backRemoved.*(uint8(S1_10Centre)))
+% colorbar
+% subplot(154)
+% imagesc(dataIn_backRemoved.*(uint8(S1_10Right)))
+% colorbar
+% subplot(155)
+% imagesc(dataIn)
+%%
+% figure(11)
+% subplot(151)
+% imagesc(peaks_Edges)
+% colorbar
+% subplot(152)
+% imagesc(peaks_Left)
+% colorbar
+% subplot(153)
+% imagesc(peaks_Centre)
+% colorbar
+% subplot(154)
+% imagesc(peaks_Right)
+% colorbar
+% 
+% 
+% colormap jet
+
+dataOut         = dataIn;
+dataOut(:,:,2) = dataOut(:,:,2) +38.71793*uint8(allPeaks);
+
 
 %%
 figure
-subplot(151)
-imagesc(dataIn.*repmat(uint8(S1_10Edges),[1 1 3]))
-subplot(152)
-imagesc(dataIn.*repmat(uint8(S1_10L),[1 1 3]))
-subplot(153)
-imagesc(dataIn.*repmat(uint8(S1_10Cent),[1 1 3]))
-subplot(154)
-imagesc(dataIn.*repmat(uint8(S1_10R),[1 1 3]))
-subplot(155)
-imagesc(dataIn)
-%% Display
-figure
-S1B = dataIn;
-S1B(:,:,2) = S1B(:,:,2) +38.71793*uint8(S1_10L+S1_10R);
-S1B(:,:,1) = S1B(:,:,1) +38.71793*uint8(regionIris);
-S1B(S1B>255) = 255;
-%imagesc(S1_1+25*uint8(S1_10L+S1_10R))
-%colormap jet
-imagesc(S1B)
-axis equal
-axis([1 cols1 1 rows1])
-set(gca,'position',[0 0 1 1 ]);axis off
-set(gcf,'position',[10 10 10+cols1 10+rows1])
+subplot(161)
+imagesc(dataOut.*repmat(uint8(S1_10Edges),[1 1 3]))
+subplot(162)
+imagesc(dataOut.*repmat(uint8(S1_10Left),[1 1 3]))
+subplot(163)
+imagesc(dataOut.*repmat(uint8(S1_10Centre),[1 1 3]))
+subplot(164)
+imagesc(dataOut.*repmat(uint8(S1_10Right),[1 1 3]))
+subplot(165)
+imagesc(dataOut)
+subplot(166)
+imagesc(bwlabel(allPeaks))
 
+ set(gcf,'position',[100 400 1000 500])
+ qqq=1;
+%% Display
+% figure
+% S1B = dataIn;
+% S1B(:,:,2) = S1B(:,:,2) +38.71793*uint8(S1_10Left+S1_10Right);
+% S1B(:,:,1) = S1B(:,:,1) +38.71793*uint8(regionIris);
+% S1B(S1B>255) = 255;
+% %imagesc(S1_1+25*uint8(S1_10Left+S1_10Right))
+% %colormap jet
+% imagesc(S1B)
+% axis equal
+% axis([1 cols1 1 rows1])
+% set(gca,'position',[0 0 1 1 ]);axis off
+% set(gcf,'position',[10 10 10+cols1 10+rows1])
+% 
 
 
 
